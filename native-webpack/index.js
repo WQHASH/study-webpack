0e1233af37897199ebd4f1c5ab44ff943e47ebef
wqhash <wqhash@163.com>
2020-05-07 20:54:16 +0800

    【修改webpack配置】

--- native-webpack/index.js
+++ native-webpack/index.js
@@ -2,7 +2,14 @@
  * @Description: 
  * @Author: wangqi
  * @Date: 2020-05-05 21:59:09
- * @LastEditTime: 2020-05-05 22:00:09
+ * @LastEditTime: 2020-05-07 20:41:53
  */
 let result = require("./base/a.js");
 console.log(result);
+
+
+/**
+ *  __dirname: 目的在于处理不同操作系统的反斜杠问题
+ */
+const { resolve } = require("path");
+console.log(resolve(__dirname, "src/index.js"));
